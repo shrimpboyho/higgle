577e761cd0f34fdeb7a8ce80262808c4d4bdb195
shrimpydubsy <new.joseph10@gmail.com>
2013-11-19 18:18:05 -0500

    Update higgle.js

--- higgle.js
+++ higgle.js
@@ -3,16 +3,57 @@
     Array.prototype.insert = function(element){
         this.push(element);
     };
+    Array.prototype.matchQuery = function(doc, query) {
+        // Console log the query
+        console.log('Query:');
+        console.log(query);
+        // Tokenize the query
+        var queryKeys = Object.keys(query);
+        var queryValues = [];
+        var i;
+        for(i = 0; i < queryKeys.length; i++){
+            queryValues.push(query[queryKeys[i]]);
+        }
+        // Tokenize the document
+        var docKeys = Object.keys(doc);
+        var docValues = [];
+        for(i = 0; i < docKeys.length; i++){
+            docValues.push(doc[docKeys[i]]);
+        }
+        // Begin scanning the doc for matches
+        var k;
+        for(i = 0; i < docKeys.length; i++){
+            for(k = 0; k < queryKeys.length; k++){
+                if(docKeys[i] == queryKeys[k]){
+                    if(docValues[i] == queryValues[k]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+        
+    };
     Array.prototype.find = function(query){
-        if(!query){
+        // handle returnign any empty queries
+        if(!query || JSON.stringify(query) === '{}'){
             return this;
         }
+        // handle any real queries
         else{
+            // this is the result array
+            // it will be packed with matching json documents
             var result = [];
+            
+            // loop through each document in the collection
             var i;
             for(i = 0; i < this.length; i++){
-                if (JSON.stringify(query) === JSON.stringify(this[i])){
-                    result.push(this[i]);
+                // console log the current document we are testing
+                console.log('Current document:');
+                var currentDoc = this[i];
+                console.log(currentDoc);
+                if (result.matchQuery(currentDoc,query)){
+                    result.push(currentDoc);
                 }
             }
             if(result.length !== 0){
@@ -23,6 +64,7 @@
             }
         }
     };
+    
     window.Higgle = function Higgle() {
         this.collections = [];
     };
@@ -46,7 +88,21 @@
 
 var db = new Higgle();
 db.createCollection("Books");
-var books = db.collection("Books");
-books.insert({'Harry Potter':'JK Rowling'});
-books.insert({'Swag':'Newlyn Joseph'});
-console.log(books.find({'Harry Potter':'JK Rowling'})[0]);
+var esums = db.collection("Books");
+esums.insert({'DOC': 1,
+              'Principal':'Blue',
+              'Dean of Strapping':'Newlyn Joseph',
+              'IPs':[5234, 7432]
+             });
+esums.insert({'DOC': 2,
+              'Harry Potter':'JK Rowling',
+              'Swag':'Newlyn Joseph'
+             });
+esums.insert({'DOC': 3,
+              'Principal':'Blue',
+              'Dean of Strapping':'Newlyn Joseph',
+              'IPs':[5234, 7432]
+             });
+var queryresults = esums.find({'Principal':'Blue'});
+console.log("Matching documents");
+console.log(queryresults);
