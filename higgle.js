b0316bb156581c19f9af382f8cba64eca682a484
shrimpydubsy <new.joseph10@gmail.com>
2013-11-19 15:37:27 -0500

    Update higgle.js

--- higgle.js
+++ higgle.js
@@ -1,4 +1,26 @@
 (function(){
 
+    window.Higgle = function Higgle() {
+        this.collections = [];
+    };
+    Higgle.prototype.createCollection = function(name) {
+        this.collections.push([name,[]]);
+    };
+    Higgle.prototype.collection = function(name) {
+        var i;
+        for(i = 0; i < this.collections.length; i++){
+            if(name = this.collections[i][0]){
+                return this.collections[i];
+            }
+        }
+        return false;
+    };
+    Higgle.prototype.close = function() {
+        return true;
+    };
 
 })();
+
+var db = new Higgle();
+db.createCollection("Books");
+console.log(db.collection("Books"));
